39380b31d377bf5fd8489f17817a0b7486067a7e
nktnvrvskj <nukitankt@gmail.com>
2026-09-07 13:59:48 +0300

    update

--- main.py
+++ main.py
@@ -1,4 +1,34 @@
-﻿is_start = True
+﻿task = []
+is_start = True
+def clear():
+    print("\033[H\033[J", end="")
+def task_print():
+    i = 0
+    for key in task:
+        i +=1
+        print(i,"задача:",key)
 while(is_start):
-    print("start")
-    is_start = False
+    clear()
+    print("менеджер задач")
+    print("1 - посмотреть задачи")
+    print("2 - добавить задачи")
+    print("3 - удалить задачу")
+    print("4 - выйти из приложени")
+    a = input(">>>")
+    if a == 1:
+        task_print()
+        input("нажмите Enter чтобы продолжить")
+    elif a == 2:
+        print("ведите задачу")
+        task.append(input(">>>"))
+        task_print()
+        input("нажмите Enter чтобы продолжить")
+    elif a == 3:
+        print("ведите номер задачи")
+        task_print()
+        task.pop(1-int(input(">>>")))
+        task_print()
+        input("нажмите Enter чтобы продолжить")
+    elif a == 4:
+        is_start = False
+    else:print("такого пункта нет");input("нажмите Enter чтобы продолжить")
